b0b2109fadb6f5d88ff06b18674b6ed3bf540390
RDDcat <yukmaro@gmail.com>
2023-02-21 23:28:05 +0900

    [UPDATE] Make HTML pages and add nav function

---
+++ todo/public/js/nav.js
@@ -0,0 +1,19 @@
+function navHome(){
+    location.href='index.html';
+}
+
+function navTodo(){
+    location.href='todo.html';
+}
+
+function navMemo(){
+    location.href='memo.html';
+}
+
+function navBoard(){
+    location.href='board.html';
+}
+
+function navAbout(){
+    location.href='about.html';
+}
