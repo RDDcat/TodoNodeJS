ea1a7dcc9b91c3f27cf2f0e7497603f83e45aea6
RDDcat <yukmaro@gmail.com>
2023-02-28 21:57:40 +0900

    [UPDATE] Memo create form tag with js, css

--- todo/public/js/nav.js
+++ todo/public/js/nav.js
@@ -1,24 +1,19 @@
 function navHome(){
     location.href='index.html';
-    alert('home');
 }
 
 function navTodo(){
     location.href='todo.html';
-    alert('todo');
 }
 
 function navMemo(){
     location.href='memo.html';
-    alert('memo');
 }
 
 function navBoard(){
     location.href='board.html';
-    alert('board');
 }
 
 function navAbout(){
     location.href='about.html';
-    alert('about');
 }
