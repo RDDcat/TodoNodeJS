b0c2b8c7b0c4b6ea6fc006db15742c6756893846
RDDcat <yukmaro@gmail.com>
2023-04-04 01:14:00 +0900

    [UPDATE] 보드 디테일 데이터 연결

--- todo/public/js/board.js
+++ todo/public/js/board.js
@@ -1,4 +1,8 @@
 const todo_list = document.querySelector(".list");
+const modal_memo = document.querySelector(".modal-memo");
+const modal_overlay = document.querySelector(".modal-overlay");
+const modal_title = document.querySelector(".modal-title");
+const modal_body = document.querySelector(".modal-body");
 
 
 async function setUpBoard(){
@@ -27,6 +31,7 @@ function setUp(data){
         var temp = document.createElement("li");
         temp.className = "item";
         temp.draggable = true;
+        temp.id = data[i].id;
         temp.innerHTML = my_var;
         todo_list.appendChild(temp);
     }
@@ -34,12 +39,40 @@ function setUp(data){
     // 클릭이벤트
     let items = document.querySelectorAll(".item");
     console.log("items" + items);
+
     items.forEach((item)=> item.addEventListener("click", function(){
-        ClickMemo(item);
+        showMemo(item);
     })
     );
 }
 
+// modal_overlay.addEventListener("click", function(){
+//         console.log("오버레이 클릭");
+//         modal_memo.classList.add("hidden");            
+// });
+
+async function showMemo(item){
+    console.log("item : " + item.id); 
+
+    // 데이터 가져옴
+    var url = new URL('http://127.0.0.1:8000/detailBoard');
+    var params = { id: item.id };
+    url.search = new URLSearchParams(params).toString();
+
+    var responseDetail;
+    await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        responseDetail = data;
+    });
+    console.log('responseDetail : ', responseDetail);
+
+    modal_title.innerHTML = responseDetail.id;
+    modal_body.innerHTML = responseDetail.content;
+
+    // 보이기
+    modal_memo.classList.remove("hidden");       
+}; 
 
 
 
