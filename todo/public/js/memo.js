495c7be7a3151480edea7f802e3cbc9ced91696d
RDDcat <yukmaro@gmail.com>
2023-04-11 18:23:36 +0900

    [FIX] 메모 저장기능 버그 수정 (정상 작동)

--- todo/public/js/memo.js
+++ todo/public/js/memo.js
@@ -4,10 +4,16 @@ form.addEventListener("submit", (event) => {
     event.preventDefault(); // prevent the form from submitting normally
 
     const formData = new FormData(form);
+    const formDataObj = Object.fromEntries(formData.entries());
+
+    console.log(formDataObj);
 
     fetch("http://ec2-18-183-36-88.ap-northeast-1.compute.amazonaws.com:8000/board", {
+        headers: {
+            "Content-Type": "application/json"
+        },
         method: "POST",
-        body: formData
+        body: JSON.stringify(formDataObj)
     })
     .then(response => response.json())
     .then(data => console.log(data))
